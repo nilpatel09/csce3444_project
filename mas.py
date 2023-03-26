6562bb82196665942c4e89d5489586a1acdcf25c
nilpatel09 <124824455+nilpatel09@users.noreply.github.com>
2023-03-25 20:28:19 -0500

    added important libraries
    Required libraries are imported to use.

---
+++ mas.py
@@ -0,0 +1,15 @@
+# import libraries#
+
+import tkinter as tk
+import tkinter.simpledialog as tsd
+import csv
+import time
+import datetime
+import os
+
+# import elements of libraries
+
+from tkinter import messagebox as mess
+from tkinter import ttk
+
+# tkinter window design [GUI]
